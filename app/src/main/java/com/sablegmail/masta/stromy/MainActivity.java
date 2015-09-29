7d390e9af31a98c76f88b496922fe10a4f6aa845
Aleksandr Sobol <bonpet666@gmail.com>
2015-09-29 16:52:55 +0300

    Добавлен OkHttp

--- app/src/main/java/com/sablegmail/masta/stromy/MainActivity.java
+++ app/src/main/java/com/sablegmail/masta/stromy/MainActivity.java
@@ -2,37 +2,43 @@ package com.sablegmail.masta.stromy;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
 
 
 public class MainActivity extends ActionBarActivity {
 
-    @Override
+    public static final String TAG = MainActivity.class.getSimpleName();
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        String apiKey = "1c737001312c4beec2183c6e19ddc204";
+        double latitude = 37.8267;
+        double longtitude = -122.423;
+        String forecastURL = "https://api.forecast.io/forecast/" +apiKey
+                +   "/" + latitude+ ", " + longtitude;
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(forecastURL)
+                .build();
+        Call call = client.newCall(request);
+        try {
+            Response response = call.execute();
+            if (response.isSuccessful()){
+                Log.v(TAG, response.body().string());
+            }
+        } catch (IOException e) {
+            Log.e(TAG, "Exception caught: ");
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
