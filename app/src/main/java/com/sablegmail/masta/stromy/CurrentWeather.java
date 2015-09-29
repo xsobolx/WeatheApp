602ef9eacaa456dd025586851aa0b2f1621dd13c
Aleksandr Sobol <bonpet666@gmail.com>
2015-09-29 20:16:47 +0300

    Добавлена модель CurrentWeather

---
+++ app/src/main/java/com/sablegmail/masta/stromy/CurrentWeather.java
@@ -0,0 +1,154 @@
+package com.sablegmail.masta.stromy;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+/**
+ * Created by admin on 29.09.2015.
+ */
+public class CurrentWeather {
+    private String mIcon;
+    private long mTime;
+    private double mTemperature;
+    private double mHumidity;
+    private double mPrecipeChance;
+    private String mSummary;
+    private String mTimeZone;
+
+    public String getTimeZone() {
+        return mTimeZone;
+    }
+
+    public void setTimeZone(String timeZone) {
+        mTimeZone = timeZone;
+    }
+
+    public String getIcon() {
+        return mIcon;
+    }
+
+    public void setIcon(String icon) {
+        mIcon = icon;
+    }
+
+    public int getIconId(){
+        int iconId = R.drawable.clear_day;
+
+        switch (mIcon){
+            case "clear-day":
+                iconId = R.drawable.clear_day;
+                break;
+            case "clear-night":
+                iconId = R.drawable.clear_night;
+                break;
+            case "rain":
+                iconId = R.drawable.rain;
+                break;
+            case "snow":
+                iconId = R.drawable.snow;
+                break;
+            case "sleet":
+                iconId = R.drawable.sleet;
+                break;
+            case "wind":
+                iconId = R.drawable.wind;
+                break;
+            case "fog":
+                iconId = R.drawable.fog;
+                break;
+            case "cloudy":
+                iconId = R.drawable.cloudy;
+                break;
+            case "partly-cloudy-day":
+                iconId = R.drawable.partly_cloudy;
+                break;
+            case "partly-cloudy-night":
+                iconId = R.drawable.cloudy_night;
+                break;
+            default:
+                break;
+        }
+
+//        if (mIcon.equals("clear-day")) {
+//            iconId = R.drawable.clear_day;
+//        }
+//        else if (mIcon.equals("clear-night")) {
+//            iconId = R.drawable.clear_night;
+//        }
+//        else if (mIcon.equals("rain")) {
+//            iconId = R.drawable.rain;
+//        }
+//        else if (mIcon.equals("snow")) {
+//            iconId = R.drawable.snow;
+//        }
+//        else if (mIcon.equals("sleet")) {
+//            iconId = R.drawable.sleet;
+//        }
+//        else if (mIcon.equals("wind")) {
+//            iconId = R.drawable.wind;
+//        }
+//        else if (mIcon.equals("fog")) {
+//            iconId = R.drawable.fog;
+//        }
+//        else if (mIcon.equals("cloudy")) {
+//            iconId = R.drawable.cloudy;
+//        }
+//        else if (mIcon.equals("partly-cloudy-day")) {
+//            iconId = R.drawable.partly_cloudy;
+//        }
+//        else if (mIcon.equals("partly-cloudy-night")) {
+//            iconId = R.drawable.cloudy_night;
+//        }
+
+        return iconId;
+    }
+
+    public long getTime() {
+        return mTime;
+    }
+
+    public String getFormatedTime(){
+        SimpleDateFormat formatter = new SimpleDateFormat("h:mm a");
+        formatter.setTimeZone(TimeZone.getTimeZone(getTimeZone()));
+        Date dateTime = new Date(getTime());
+        String timeString = formatter.format(dateTime);
+        return timeString;
+    }
+
+    public void setTime(long time) {
+        mTime = time;
+    }
+
+    public double getTemperature() {
+        return mTemperature;
+    }
+
+    public void setTemperature(double temperature) {
+        mTemperature = temperature;
+    }
+
+    public double getHumidity() {
+        return mHumidity;
+    }
+
+    public void setHumidity(double humidity) {
+        mHumidity = humidity;
+    }
+
+    public double getPrecipeChance() {
+        return mPrecipeChance;
+    }
+
+    public void setPrecipeChance(double precipeChance) {
+        mPrecipeChance = precipeChance;
+    }
+
+    public String getSummary() {
+        return mSummary;
+    }
+
+    public void setSummary(String summary) {
+        mSummary = summary;
+    }
+}
--- app/src/main/java/com/sablegmail/masta/stromy/MainActivity.java
+++ app/src/main/java/com/sablegmail/masta/stromy/MainActivity.java
@@ -6,7 +6,6 @@ import android.net.NetworkInfo;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.Toast;
 
 import com.squareup.okhttp.Call;
 import com.squareup.okhttp.Callback;
@@ -14,21 +13,25 @@ import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
 
 
 public class MainActivity extends ActionBarActivity {
 
     public static final String TAG = MainActivity.class.getSimpleName();
+    private CurrentWeather mCurrentWeather;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         String apiKey = "1c737001312c4beec2183c6e19ddc204";
-        double latitude = 37.8267;
-        double longtitude = -122.423;
+        double latitude = 55.488091;
+        double longitude = 36.033576;
         String forecastURL = "https://api.forecast.io/forecast/" + apiKey
-                + "/" + latitude + ", " + longtitude;
+                + "/" + latitude + ", " + longitude;
         if(isNetworkAvailable()) {
             OkHttpClient client = new OkHttpClient();
             Request request = new Request.Builder()
@@ -44,13 +47,16 @@ public class MainActivity extends ActionBarActivity {
                 @Override
                 public void onResponse(Response response) throws IOException {
                     try {
-                        Log.v(TAG, response.body().string());
+                        String jsonData = response.body().string();
+                        Log.v(TAG, jsonData);
                         if (response.isSuccessful()) {
-
+                            mCurrentWeather = getCurrentDetails(jsonData);
                         } else {
-                            alertUserAboutConnectioFailError();
+                            alertUserAboutConnectionFailError();
                         }
                     } catch (IOException e) {
+                        Log.e(TAG, "Exception caught: ");
+                    } catch (JSONException e) {
                         Log.e(TAG, "Exception caught: ");
                     }
                 }
@@ -59,6 +65,27 @@ public class MainActivity extends ActionBarActivity {
             alertUserAboutNetworkUnavailableError();
         }
         Log.d(TAG, "Main UI code is running!");
+    }
+
+    private CurrentWeather getCurrentDetails(String jsonData) throws JSONException {
+        JSONObject forecast = new JSONObject(jsonData);
+        String timeZone = forecast.getString("timezone");
+        Log.i(TAG, "From JSON: " + timeZone);
+
+        JSONObject currently = forecast.getJSONObject("currently");
+
+        CurrentWeather currentWeather = new CurrentWeather();
+        currentWeather.setHumidity(currently.getDouble("humidity"));
+        currentWeather.setTime(currently.getLong("time"));
+        currentWeather.setIcon(currently.getString("icon"));
+        currentWeather.setPrecipeChance(currently.getDouble("precipProbability"));
+        currentWeather.setSummary(currently.getString("summary"));
+        currentWeather.setTemperature(currently.getDouble("temperature"));
+        currentWeather.setTimeZone(timeZone);
+
+        Log.d(TAG, currentWeather.getFormatedTime());
+
+        return currentWeather;
     }
 
     private boolean isNetworkAvailable(){
@@ -72,7 +99,7 @@ public class MainActivity extends ActionBarActivity {
         return isAvailable;
     }
 
-    private void alertUserAboutConnectioFailError() {
+    private void alertUserAboutConnectionFailError() {
         ConnectionFailDialogFragment dialog = new ConnectionFailDialogFragment();
         dialog.show(getFragmentManager(), "error_dialog");
     }
