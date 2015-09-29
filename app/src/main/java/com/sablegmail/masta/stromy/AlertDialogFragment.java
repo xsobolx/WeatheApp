c1cdffed68352635e8076b1df3e7e4c6e6326c00
Aleksandr Sobol <bonpet666@gmail.com>
2015-09-29 17:30:03 +0300

    Настроено OkHttp соединение с ForeCast

---
+++ app/src/main/java/com/sablegmail/masta/stromy/AlertDialogFragment.java
@@ -0,0 +1,7 @@
+package com.sablegmail.masta.stromy;
+
+/**
+ * Created by admin on 29.09.2015.
+ */
+public class AlertDialogFragment {
+}
--- app/src/main/java/com/sablegmail/masta/stromy/MainActivity.java
+++ app/src/main/java/com/sablegmail/masta/stromy/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
@@ -24,21 +25,38 @@ public class MainActivity extends ActionBarActivity {
         String apiKey = "1c737001312c4beec2183c6e19ddc204";
         double latitude = 37.8267;
         double longtitude = -122.423;
-        String forecastURL = "https://api.forecast.io/forecast/" +apiKey
-                +   "/" + latitude+ ", " + longtitude;
+        String forecastURL = "https://api.forecast.io/forecast/" + apiKey
+                + "/" + latitude + ", " + longtitude;
 
         OkHttpClient client = new OkHttpClient();
         Request request = new Request.Builder()
                 .url(forecastURL)
                 .build();
         Call call = client.newCall(request);
-        try {
-            Response response = call.execute();
-            if (response.isSuccessful()){
-                Log.v(TAG, response.body().string());
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+
             }
-        } catch (IOException e) {
-            Log.e(TAG, "Exception caught: ");
-        }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+                try {
+                    Log.v(TAG, response.body().string());
+                    if (response.isSuccessful()){
+
+                    } else {
+                        alertUserAboutError();
+                    }
+                } catch (IOException e) {
+                    Log.e(TAG, "Exception caught: ");
+                }
+            }
+        });
+
+        Log.d(TAG, "Main UI code is running!");
+    }
+
+    private void alertUserAboutError() {
     }
 }
